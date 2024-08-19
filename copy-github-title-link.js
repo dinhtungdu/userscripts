bbedc718324b29a1c46f9a2cb31f71b2d8cea332
Tung Du <dinhtungdu@gmail.com>
2024-08-19 09:40:50 +0700

    Create copy-github-title-link.js

---
+++ copy-github-title-link.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Copy GitHub Issue/PR Title Link
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Copy the title link of the current GitHub issue/PR when you press "c" followed by "c"
+// @author       Tung Du
+// @match        https://github.com/*/*/issues/*
+// @match        https://github.com/*/*/pull/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let keysPressed = [];
+
+    document.addEventListener('keydown', function(e) {
+        keysPressed.push(e.key);
+
+        if (keysPressed.length > 2) {
+            keysPressed.shift();
+        }
+
+        if (keysPressed[0] === 'c' && keysPressed[1] === 'c') {
+            const titleElement = document.querySelector('.gh-header-title');
+            if (titleElement) {
+                const title = titleElement.textContent.trim();
+                const url = window.location.href;
+                const htmlLink = `<a href="${url}">${title}</a>`;
+
+                copyToClipboard(htmlLink, title);
+            }
+
+            keysPressed = []; // Reset the keys
+        }
+    });
+    function copyToClipboard(html,plain) {
+        const blob = new Blob([html], { type: 'text/html' });
+        const blobText = new Blob([plain], {type: 'text/plain'});
+        const data = [new ClipboardItem({ 'text/html': blob, 'text/plain': blobText })];
+
+        navigator.clipboard.write(data).then(function() {
+            console.log('HTML link copied to clipboard.');
+        }).catch(function(error) {
+            console.error('Failed to copy HTML link: ', error);
+        });
+    }
+})();
