1bbc622542cb3816a6824307db51a5a67e76a36b
Tung Du <dinhtungdu@gmail.com>
2024-09-23 16:48:12 +0700

    feat: w-e to copy only issue/PR number with link

--- copy-github-title-link.user.js
+++ copy-github-title-link.user.js
@@ -8,40 +8,61 @@
 // @match        https://github.com/*/*/pull/*
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    let keysPressed = [];
+  let keysPressed = [];
 
-    document.addEventListener('keydown', function(e) {
-        keysPressed.push(e.key);
+  document.addEventListener("keydown", function (e) {
+    keysPressed.push(e.key);
 
-        if (keysPressed.length > 2) {
-            keysPressed.shift();
-        }
-
-        if (keysPressed[0] === 'w' && keysPressed[1] === 'w') {
-            const titleElement = document.querySelector('.gh-header-title');
-            if (titleElement) {
-                const title = titleElement.textContent.trim().replace(/(\r\n|\n|\r)/gm, "").replace(/ +(?= )/g,'');
-                const url = window.location.href;
-                const htmlLink = `<a href="${url}">${title}</a>`;
-
-                copyToClipboard(htmlLink, title);
-            }
-
-            keysPressed = []; // Reset the keys
-        }
-    });
-    function copyToClipboard(html,plain) {
-        const blob = new Blob([html], { type: 'text/html' });
-        const blobText = new Blob([plain], {type: 'text/plain'});
-        const data = [new ClipboardItem({ 'text/html': blob, 'text/plain': blobText })];
-
-        navigator.clipboard.write(data).then(function() {
-            console.log('HTML link copied to clipboard.');
-        }).catch(function(error) {
-            console.error('Failed to copy HTML link: ', error);
-        });
+    if (keysPressed.length > 2) {
+      keysPressed.shift();
     }
+
+    if (keysPressed[0] === "w" && keysPressed[1] === "w") {
+      const titleElement = document.querySelector(".gh-header-title");
+      if (titleElement) {
+        const title = titleElement.textContent
+          .trim()
+          .replace(/(\r\n|\n|\r)/gm, "")
+          .replace(/ +(?= )/g, "");
+        const url = window.location.href;
+        const htmlLink = `<a href="${url}">${title}</a>`;
+
+        copyToClipboard(htmlLink, title);
+      }
+
+      keysPressed = []; // Reset the keys
+    }
+
+    if (keysPressed[0] === "w" && keysPressed[1] === "e") {
+      const url = window.location.href;
+      const issueNumber = url.match(/\d+/)[0];
+      if (issueNumber) {
+        copyToClipboard(
+          `<a href="${url}">#${issueNumber}</a>`,
+          `#${issueNumber}`,
+        );
+      }
+
+      keysPressed = []; // Reset the keys
+    }
+  });
+  function copyToClipboard(html, plain) {
+    const blob = new Blob([html], { type: "text/html" });
+    const blobText = new Blob([plain], { type: "text/plain" });
+    const data = [
+      new ClipboardItem({ "text/html": blob, "text/plain": blobText }),
+    ];
+
+    navigator.clipboard
+      .write(data)
+      .then(function () {
+        console.log("HTML link copied to clipboard.");
+      })
+      .catch(function (error) {
+        console.error("Failed to copy HTML link: ", error);
+      });
+  }
 })();
