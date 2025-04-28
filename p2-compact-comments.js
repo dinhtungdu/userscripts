76209bd1cc9d98e09555dfb5b6bedc9bea491084
Tung Du <dinhtungdu@gmail.com>
2025-04-28 08:53:12 +0700

    style: more compact

--- p2-compact-comments.js
+++ p2-compact-comments.js
@@ -12,10 +12,10 @@
 (function () {
   "use strict";
   const css = `
-    .o2-post-comments .comment-content {
+.o2-post-comments .comment-content {
   font-size: 0.875em !important;
   margin-left: 0em !important;
-  margin-top: 0.5em !important;
+  margin-top: 0em !important;
 }
 .o2-post-comments .comment-content p {
   margin-bottom: 1rem !important;
@@ -24,8 +24,19 @@
   padding: 1em 1em 0.1em !important;
   margin-bottom: 1em !important;
 }
+header.o2-comment-header {
+  padding: 0 !important;
+}
+.o2-comment .o2-comment-footer-actions p {
+  margin-bottom: 0em !important;
+}
 .o2-child-comments {
+  margin-top: 0.5em !important;
   padding-left: 1.5em !important;
+  border-left: 1px solid;
+  border-bottom: 1px solid;
+  margin-bottom: -1px;
+  border-color: #f2f2f2;
 }
 .o2-child-comments .o2-comment-header .fragment-avatar-wrapper,
 .o2-child-comments .o2-comment-header .avatar {
@@ -33,13 +44,10 @@
   height: 20px !important;
 }
 .o2-child-comments .o2-comment {
-  padding: 0.5em 0 0 !important;
+  padding: 0 !important;
 }
-.o2-child-comments {
-    border-left: 1px solid;
-    border-bottom: 1px solid;
-    margin-bottom: -1px;
-    border-color: #f2f2f2;
+.o2-child-comments .o2-comment + .o2-comment {
+  margin-top: 0.5em;
 }
     `;
   GM_addStyle(css);
